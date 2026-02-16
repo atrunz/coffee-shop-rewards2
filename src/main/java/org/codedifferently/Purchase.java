6e85e40ec1aba468152761abc6d0657dda7c2f13
atrunz <atrunzo261@gmail.com>
2026-02-16 15:01:30 -0500

    purchase class fields + getter/setters created

--- src/main/java/org/codedifferently/Purchase.java
+++ src/main/java/org/codedifferently/Purchase.java
@@ -1,4 +1,37 @@
 package org.codedifferently;
 
 public class Purchase {
+    private String itemName;
+    private double price;
+    private boolean isDrink;
+
+    public Purchase(String itemName, double price, boolean isDrink){
+        this.itemName = itemName;
+        this.price = price;
+        this.isDrink = isDrink;
+    }
+
+    public boolean isDrink() {
+        return isDrink;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setDrink(boolean drink) {
+        isDrink = drink;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
